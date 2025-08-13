289170492db788b5bb9cec12da82d61928203a0e
Manuelli Flaviano <manuelli.flaviano@germinare.org.br>
2025-08-13 15:29:24 -0300

    Primeiro commit do projeto Java

---
+++ src/main/java/org/example/scaneia_dsii/ScaneIaDsiiApplication.java
@@ -0,0 +1,13 @@
+package org.example.scaneia_dsii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaneIaDsiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaneIaDsiiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scaneia_dsii/ScaneIaDsiiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scaneia_dsii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaneIaDsiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

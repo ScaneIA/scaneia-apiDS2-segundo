3da44001597b0f714643a0a5db445976a01d0a8e
Brenda <brend4costa.1405@gmail.com>
2025-10-09 14:02:55 -0300

    fazendo classes de responseDTO de estrutura, usuariotipo, transacao e transacaoitem

--- src/main/java/org/example/scaneia_dsii/dtos/UsuarioTipoResponseDTO.java
+++ src/main/java/org/example/scaneia_dsii/dtos/UsuarioTipoResponseDTO.java
@@ -1,4 +1,14 @@
 package org.example.scaneia_dsii.dtos;
 
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UsuarioTipoResponseDTO {
+
+    private Integer id;
+    private String descricao;
+    private Boolean ativo;
 }

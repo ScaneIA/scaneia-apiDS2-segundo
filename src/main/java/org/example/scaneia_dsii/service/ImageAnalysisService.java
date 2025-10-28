01486f9955d033c32f854eab32c19ef8e30f688b
mateusmca2905@gmail.com <mateus.araujo@germinare.org.br>
2025-10-28 07:52:47 -0300

    feat(azure): add ocr

---
+++ src/main/java/org/example/scaneia_dsii/service/ImageAnalysisService.java
@@ -0,0 +1,41 @@
+package org.example.scaneia_dsii.service;
+
+import com.azure.ai.vision.imageanalysis.*;
+import com.azure.ai.vision.imageanalysis.models.*;
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.*;
+
+@Service
+public class ImageAnalysisService {
+
+    @Value("${azure.vision.endpoint}")
+    private String endpoint;
+
+    @Value("${azure.vision.key}")
+    private String subscriptionKey;
+
+    public Map<String, Object> analyzeImage(MultipartFile file) throws IOException {
+        ImageAnalysisClient client = new ImageAnalysisClientBuilder()
+                .endpoint(endpoint)
+                .credential(new AzureKeyCredential(subscriptionKey))
+                .buildClient();
+
+        ImageAnalysisOptions options = new ImageAnalysisOptions();
+
+        ImageAnalysisResult result = client.analyze(
+                BinaryData.fromStream(file.getInputStream()),
+                Arrays.asList(VisualFeatures.READ),
+                options
+        );
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("fullResult", result);
+        return response;
+    }
+}
